bbdfd835fc87a7c7ba7846fa62ad9231a4a8e6b0
zuczekann <127730541+zuczekann@users.noreply.github.com>
2023-03-22 17:54:51 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+let zmiennaP = document.getElementById("opis");
+    //zmiennaP.textContent = "dowolny tekst wstawiony do paragrafu"
+
+
+let button = document.querySelector("button");
+    button.onclick = function() {
+        zmiennaP.textContent = "dowolny tekst wstawiony do paragrafu"
+    }
+
+
+
